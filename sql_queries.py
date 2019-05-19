880550c3c9cf7878cc9a5fb1a89415afc0e95f69
Shivam Gupta <sgupta@godaddy.com>
2019-05-18 23:26:41 -0700

    users tables on conflict update

--- sql_queries.py
+++ sql_queries.py
@@ -76,7 +76,7 @@ songplay_table_insert = "INSERT INTO songplays (songplay_id, start_time, user_id
 user_table_insert = "INSERT INTO users (user_id, first_name, last_name, gender, level)      \
                      VALUES (%s, %s, %s, %s, %s)                                            \
                      ON CONFLICT (user_id)                                                  \
-                     DO UPDATE SET level=EXCLUDED.level"
+                     DO UPDATE SET level=EXCLUDED.level;"
 
 song_table_insert = "INSERT INTO songs (song_id, title, artist_id, year, duration)          \
                      VALUES (%s, %s, %s, %s, %s)                                            \
